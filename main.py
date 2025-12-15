ec8b2075d2a5504dbeb97fa0a486705728de7e94
Luís Eduardo <cavalcantesantosluiseduardo@gmail.com>
2025-12-15 20:33:25 -0300

    processing inputs

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from core import *
+from schedulers import *
+
 entradas = []
 
 with open("EntradaProcessos.txt", "r") as arquivo:
@@ -6,8 +9,15 @@ with open("EntradaProcessos.txt", "r") as arquivo:
 
 nProc, quantum, tTroca = int(entradas[0][0]), int(entradas[0][1]), int(entradas[0][2])
 
+lista_processos = []
 
-for i in range(nProc):
-    #pega as infos e joga na classe
-    pass
+for i in range(1, nProc):
+    pid = int(entradas[i][0])
+    creation_time = int(entradas[i][1])
+    priority = int(entradas[i][2])
+    cpu_time = int(entradas[i][3])
 
+    lista_processos.append(Process(pid, priority, creation_time, cpu_time))
+
+#RoundRobinScheduler(tTroca, quantum, lista_processos)
+#PriorityScheduler(tTroca, quantum, lista_processos)
