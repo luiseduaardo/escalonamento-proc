64b2ea4fb331ee79072d6b9638dfd1ee042985a2
Luan Gustavo <lgns@cin.ufpe.br>
2025-12-16 23:37:15 -0300

    função de print

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from schedulers import RoundRobinScheduler, PriorityScheduler
+from core import ProcessScheduler
 
 def read_file(filename):
     input = []
@@ -27,8 +28,21 @@ def read_file(filename):
     
     return quantum, ctx_switch_t, process_list
 
+def print_info(scheduler: ProcessScheduler):
+    print(scheduler.timeline)
+
+    average = 0
+    for n in scheduler.return_times:
+        average += n
+    average /= len(scheduler.return_times)
+
+    print(f"Tempo médio de retorno: {average}")
+    print(f"Tempo total: {scheduler.total_time - scheduler.overhead_time}")
+    print(f"Número de chaveamentos: {scheduler.context_switch_counter}")
+    print(f"Overhead: {(scheduler.overhead_time / scheduler.total_time):.4f}")
+
 def main():
-    quantum, ctx_switch_t, proc_list = read_file("test/1.txt")
+    quantum, ctx_switch_t, proc_list = read_file("test/2.txt")
 
     round_robin = RoundRobinScheduler(ctx_switch_t, quantum, proc_list)
     priority = PriorityScheduler(ctx_switch_t, proc_list)
@@ -36,26 +50,9 @@ def main():
     round_robin.start()
     priority.start()
 
-    print(round_robin.timeline)
-    average = 0
-    for n in round_robin.return_times:
-        average += n
-    average /= len(round_robin.return_times)
-    print(f"Tempo médio de retorno: {average}")
-    print(f"Tempo total: {round_robin.total_time - round_robin.overhead_time}")
-    print(f"Número de chaveamentos: {round_robin.context_switch_counter}")
-    print(f"Overhead: {(round_robin.overhead_time / round_robin.total_time):.4f}")
-
+    print_info(round_robin)
     print("--------------------")
-    print(priority.timeline)
-    average = 0
-    for n in priority.return_times:
-        average += n
-    average /= len(priority.return_times)
-    print(f"Tempo médio de retorno: {average}")
-    print(f"Tempo total: {priority.total_time - priority.overhead_time}")
-    print(f"Número de chaveamentos: {priority.context_switch_counter}")
-    print(f"Overhead: {(priority.overhead_time / priority.total_time):.4f}")
+    print_info(priority)
 
 if __name__ == "__main__":
     main()
