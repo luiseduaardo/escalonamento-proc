9d9c3ad6356443016de8b22ec33a42fc4e3797ca
lecs2 <lecs2@g2c19.cin.ufpe.br>
2025-12-17 06:39:34 -0300

    output formatting

---
+++ io_utils.py
@@ -0,0 +1,63 @@
+from core import *
+
+def read_file(filename):
+    input = []
+
+    with open(filename, "r") as file:
+        for linha in file:
+            input.append(linha.strip('\n').split(','))
+
+    proc_count, quantum, ctx_switch_t = int(input[0][0]), int(input[0][1]), int(input[0][2])
+
+    process_list = []
+
+    # ID,Tch,Prio,Tcpu
+    for i in range(1, proc_count+1):
+        pid = int(input[i][0])
+        creation_time = int(input[i][1])
+        priority = int(input[i][2])
+        cpu_time = int(input[i][3])
+
+        process_list.append({
+            "pid": pid,
+            "priority": priority,
+            "arrival_time": creation_time,
+            "cpu_time": cpu_time
+        })
+    
+    return quantum, ctx_switch_t, process_list
+
+def format_timeline(timeline_str):
+    if not timeline_str: return ""
+    
+    formatted = []
+    current_char = timeline_str[0]
+    start = 0
+    
+    for i, char in enumerate(timeline_str):
+        if char != current_char:
+            label = "Escalonador" if current_char == "E" else (f"Processo {current_char}" if current_char != "." else "Ocioso")
+            formatted.append(f"[{start}-{i}] {label}")
+            current_char = char
+            start = i
+            
+    label = "Escalonador" if current_char == "E" else (f"Processo {current_char}" if current_char != "." else "Ocioso")
+    formatted.append(f"[{start}-{len(timeline_str)}] {label}")
+    
+    return "\n".join(formatted)
+
+def save_file(file_obj: str, algo_name: str, scheduler: ProcessScheduler):
+    file_obj.write(f"------------ {algo_name} ------------")
+    file_obj.write("\nLINHA DO TEMPO DE OCUPAÇÃO DA CPU: ")
+    file_obj.write(scheduler.timeline+'\n')
+    file_obj.write(format_timeline(scheduler.timeline))
+
+    average = 0
+    for n in scheduler.return_times:
+        average += n
+    average /= len(scheduler.return_times)
+
+    file_obj.write(f"\n\nTempo médio de retorno: {average}")
+    file_obj.write(f"\nTempo total: {scheduler.total_time - scheduler.overhead_time}")
+    file_obj.write(f"\nNúmero de chaveamentos: {scheduler.context_switch_counter}")
+    file_obj.write(f"\nOverhead: {(scheduler.overhead_time / scheduler.total_time):.4f}")
--- main.py
+++ main.py
@@ -1,59 +1,22 @@
 from schedulers import RoundRobinScheduler, PriorityScheduler
 from core import ProcessScheduler
-
-def read_file(filename):
-    input = []
-
-    with open(filename, "r") as file:
-        for linha in file:
-            input.append(linha.strip('\n').split(','))
-
-    proc_count, quantum, ctx_switch_t = int(input[0][0]), int(input[0][1]), int(input[0][2])
-
-    process_list = []
-
-    # ID,Tch,Prio,Tcpu
-    for i in range(1, proc_count+1):
-        pid = int(input[i][0])
-        creation_time = int(input[i][1])
-        priority = int(input[i][2])
-        cpu_time = int(input[i][3])
-
-        process_list.append({
-            "pid": pid,
-            "priority": priority,
-            "arrival_time": creation_time,
-            "cpu_time": cpu_time
-        })
-    
-    return quantum, ctx_switch_t, process_list
-
-def print_info(scheduler: ProcessScheduler):
-    print("----- TIMELINE -----")
-    print(scheduler.timeline)
-
-    average = 0
-    for n in scheduler.return_times:
-        average += n
-    average /= len(scheduler.return_times)
-
-    print(f"Tempo médio de retorno: {average}")
-    print(f"Tempo total: {scheduler.total_time - scheduler.overhead_time}")
-    print(f"Número de chaveamentos: {scheduler.context_switch_counter}")
-    print(f"Overhead: {(scheduler.overhead_time / scheduler.total_time):.4f}")
+from io_utils import read_file, save_file
 
 def main():
-    quantum, ctx_switch_t, proc_list = read_file("test/2.txt")
+    input_file, output_file = "test/EntradaProcessos.txt", "test/teste.txt"
+
+    quantum, ctx_switch_t, proc_list = read_file(input_file)
 
     round_robin = RoundRobinScheduler(ctx_switch_t, quantum, proc_list)
-    priority = PriorityScheduler(ctx_switch_t, proc_list)
-
     round_robin.start()
+
+    priority = PriorityScheduler(ctx_switch_t, proc_list)
     priority.start()
 
-    print_info(round_robin)
-    print("--------------------")
-    print_info(priority)
+    with open(output_file, "w+") as file:
+        save_file(file, "ROUND ROBIN", round_robin)
+        file.write("\n\n")
+        save_file(file, "PRIORITY", priority)
 
 if __name__ == "__main__":
     main()
